b4048310253baa5ba1c69f62842a330f5f26748f
Mário R. Pinheiro <47700984+mrapinheiro@users.noreply.github.com>
2024-03-17 22:04:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def main():
     end_date = st.sidebar.date_input('Select End Date:', datetime.now())
 
     # Model selection
-    selected_model = st.sidebar.radio("Select Model", ("Neural Network", "Random Forest"))
+    selected_model = st.sidebar.radio("Select Model", ("Neural Network"))
 
     # Load stock data
     if stock_symbol:
