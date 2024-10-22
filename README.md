4dcd148532fcde5a1ae5e1df75247e1a17cbceef
Mário R. A. Pinheiro <47700984+mrapinheiro@users.noreply.github.com>
2024-10-22 22:10:07 +0100

    update_clean

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
-# Neural Network Stock Price Prediction App
+# aeon-stock-price-predict
 
-## Overview
-
-This repository hosts a stock price prediction application leveraging a neural network model. The application, built with Python, TensorFlow, and Streamlit, provides an interactive platform for users to predict stock prices based on historical data.
-
-## Features
-
-- **Neural Network Prediction**: Utilizes a trained neural network to forecast stock prices.
-- **Interactive Web Interface**: The application is powered by Streamlit, offering a user-friendly, interactive interface.
-- **Data Visualization**: Features visualizations of historical stock prices alongside predictive data, aiding in trend analysis and decision-making.
-
-## Models
-
-The neural network model is detailed in the `Models/NN.py` notebook. This notebook outlines the model's architecture, training process, and evaluation metrics. The final model is saved as `Models/NN.keras`, which the Streamlit application uses for making predictions.
-
-## Contributing
-
-We welcome contributions from the community. If you have suggestions or improvements, please fork the repository and propose your changes via a pull request.
-
-## License
-
-This project is available under the  GPL-3.0 License. For more details, see the [LICENSE](LICENSE) file.
+Current Models:
+Neural Network
--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model
 import plotly.graph_objs as go
 
 # Cache the model loading to speed up app loading
-@st.cache_resource
+@st.cache(allow_output_mutation=True)
 def load_trained_model(model_path):
     return load_model(model_path)
 
@@ -23,45 +23,28 @@ def create_dataset(data, look_back=100):
     return np.array(X)
 
 def display_charts(stock_data):
-    if stock_data.empty:
-        st.error("No data available to display charts.")
-        return
-
     # Price vs MA100
     fig1 = go.Figure()
-    fig1.add_trace(go.Scatter(
-        x=stock_data.index, y=stock_data['Close'], mode='lines', name='Close Price'))
-    if 'MA100' in stock_data.columns and stock_data['MA100'].notnull().any():
-        fig1.add_trace(go.Scatter(
-            x=stock_data.index, y=stock_data['MA100'], mode='lines', name='MA100'))
-    fig1.update_layout(title='Close Price vs MA100')
+    fig1.add_trace(go.Scatter(x=stock_data.index, y=stock_data['Close'], mode='lines', name='Close Price'))
+    fig1.add_trace(go.Scatter(x=stock_data.index, y=stock_data['MA100'], mode='lines', name='MA100'))
     st.plotly_chart(fig1, use_container_width=True)
 
     # Price vs MA100 vs MA200
-    st.subheader('30-Day Forecast')
     fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(
-        x=stock_data.index, y=stock_data['Close'], mode='lines', name='Close Price'))
-    if 'MA100' in stock_data.columns and stock_data['MA100'].notnull().any():
-        fig2.add_trace(go.Scatter(
-            x=stock_data.index, y=stock_data['MA100'], mode='lines', name='MA100'))
-    if 'MA200' in stock_data.columns and stock_data['MA200'].notnull().any():
-        fig2.add_trace(go.Scatter(
-            x=stock_data.index, y=stock_data['MA200'], mode='lines', name='MA200'))
-    fig2.update_layout(title='Close Price vs MA100 vs MA200')
+    fig2.add_trace(go.Scatter(x=stock_data.index, y=stock_data['Close'], mode='lines', name='Close Price'))
+    fig2.add_trace(go.Scatter(x=stock_data.index, y=stock_data['MA100'], mode='lines', name='MA100'))
+    fig2.add_trace(go.Scatter(x=stock_data.index, y=stock_data['MA200'], mode='lines', name='MA200'))
     st.plotly_chart(fig2, use_container_width=True)
 
     # Candlestick chart
-    candlestick_fig = go.Figure(data=[go.Candlestick(
-        x=stock_data.index,
-        open=stock_data['Open'], high=stock_data['High'],
-        low=stock_data['Low'], close=stock_data['Close'])])
+    candlestick_fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
+                                                     open=stock_data['Open'], high=stock_data['High'],
+                                                     low=stock_data['Low'], close=stock_data['Close'])])
     candlestick_fig.update_layout(title='Candlestick Chart')
     st.plotly_chart(candlestick_fig, use_container_width=True)
 
     # Volume plot
-    volume_fig = go.Figure(data=[go.Bar(
-        x=stock_data.index, y=stock_data['Volume'])])
+    volume_fig = go.Figure(data=[go.Bar(x=stock_data.index, y=stock_data['Volume'])])
     volume_fig.update_layout(title='Volume Plot')
     st.plotly_chart(volume_fig, use_container_width=True)
 
@@ -69,30 +52,19 @@ def prepare_and_predict(stock_data, model):
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(np.array(stock_data['Close']).reshape(-1, 1))
     x_pred = create_dataset(scaled_data)
-    if x_pred.size == 0:
-        st.error("Not enough data to make predictions.")
-        return None, None
-    x_pred = x_pred.reshape(x_pred.shape[0], x_pred.shape[1])  # Adjusting the reshape to match model input
+    x_pred = x_pred.reshape(x_pred.shape[0], -1)  # Adjusting the reshape to match model input
     y_pred = model.predict(x_pred)
     y_pred = scaler.inverse_transform(y_pred)
     return scaler, y_pred
 
 def display_prediction_chart(stock_data, y_pred):
-    if y_pred is None:
-        st.error("Prediction data is not available.")
-        return
     fig3 = go.Figure()
-    fig3.add_trace(go.Scatter(
-        x=stock_data.index[100:], y=stock_data['Close'][100:], mode='lines', name='Actual Price'))
-    fig3.add_trace(go.Scatter(
-        x=stock_data.index[100:], y=y_pred.flatten(), mode='lines', name='Predicted Price'))
-    fig3.update_layout(title='Actual vs Predicted Prices')
+    fig3.add_trace(go.Scatter(x=stock_data.index[100:], y=stock_data['Close'][100:], mode='lines', name='Actual Price'))
+    fig3.add_trace(go.Scatter(x=stock_data.index[100:], y=y_pred.flatten(), mode='lines', name='Predicted Price'))
+    fig3.update_layout(title='Original vs Predicted Prices')
     st.plotly_chart(fig3, use_container_width=True)
 
 def display_evaluation_metrics(stock_data, y_pred):
-    if y_pred is None:
-        st.error("Cannot display evaluation metrics without predictions.")
-        return
     y_true = stock_data['Close'].values[100:]
     mae = mean_absolute_error(y_true, y_pred)
     mse = mean_squared_error(y_true, y_pred)
@@ -101,9 +73,6 @@ def display_evaluation_metrics(stock_data, y_pred):
     st.write(f'Mean Squared Error (MSE): {mse:.2f}')
 
 def perform_and_display_forecasting(stock_data, model, scaler):
-    if scaler is None:
-        st.error("Scaler is not available for forecasting.")
-        return
     forecast_period_days = 30
     last_100_days = stock_data['Close'].tail(100).values.reshape(-1, 1)
     last_100_days_scaled = scaler.transform(last_100_days)
@@ -117,49 +86,41 @@ def perform_and_display_forecasting(stock_data, model, scaler):
 
     forecasted_prices = scaler.inverse_transform(np.array(forecasted_prices_scaled).reshape(-1, 1))
 
-    forecast_dates = pd.date_range(start=stock_data.index[-1] + timedelta(days=1),
-                                   periods=forecast_period_days, freq='D')
+    forecast_dates = pd.date_range(start=stock_data.index[-1] + timedelta(days=1), periods=forecast_period_days, freq='D')
     forecast_df = pd.DataFrame(data=forecasted_prices, index=forecast_dates, columns=['Forecast'])
 
     # Display Forecast
     st.subheader('30-Day Forecast')
     fig4 = go.Figure()
-    fig4.add_trace(go.Scatter(
-        x=forecast_df.index, y=forecast_df['Forecast'], mode='lines', name='Forecasted Price'))
+    fig4.add_trace(go.Scatter(x=forecast_df.index, y=forecast_df['Forecast'], mode='lines', name='Forecasted Price'))
     fig4.update_layout(title='Stock Price Forecast for the Next 30 Days')
     st.plotly_chart(fig4, use_container_width=True)
 
 def main():
     st.sidebar.title('Aeon Stock Price Predict')
     stock_symbol = st.sidebar.text_input('Enter Stock Ticker Symbol (e.g., MSFT):')
-    start_date = st.sidebar.date_input('Select Start Date:', datetime.now() - timedelta(days=730))
+    start_date = st.sidebar.date_input('Select Start Date:', datetime.now() - timedelta(days=365))
     end_date = st.sidebar.date_input('Select End Date:', datetime.now())
     selected_model = st.sidebar.radio("Select Model", ("Neural Network",))
 
     if stock_symbol:
         with st.spinner('Fetching stock data...'):
             stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
-
+        
         if not stock_data.empty:
             st.subheader(f'Stock Data for {stock_symbol}')
             st.write(stock_data.tail())
-
-            data_length = len(stock_data)
-            if data_length < 200:
-                st.error("Not enough data to calculate MA200. Please select a longer date range.")
-                return
-
+            
             stock_data['MA100'] = calculate_moving_average(stock_data['Close'], 100)
             stock_data['MA200'] = calculate_moving_average(stock_data['Close'], 200)
-            stock_data.dropna(inplace=True)
-
+            
             display_charts(stock_data)
 
             if selected_model == "Neural Network":
                 with st.spinner('Loading the prediction model...'):
                     model_path = 'Models/NN_model.keras'  # Ensure this path is correct
                     model = load_trained_model(model_path)
-
+                
                 scaler, y_pred = prepare_and_predict(stock_data, model)
                 display_prediction_chart(stock_data, y_pred)
                 display_evaluation_metrics(stock_data, y_pred)
