08323456afdfba0272003fa8c653aba32af0e00f
Mário R. A. Pinheiro <47700984+mrapinheiro@users.noreply.github.com>
2024-10-23 12:48:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model
 import plotly.graph_objs as go
 
 # Cache the model loading to speed up app loading
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def load_trained_model(model_path):
     return load_model(model_path)
 
